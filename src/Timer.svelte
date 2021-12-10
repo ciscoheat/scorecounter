c435390eabb704679ce7ca9bcad6f3c3d6d4635d
Andreas <ciscoheat@gmail.com>
2021-12-10 23:36:43 +0100

    Better timer.

--- src/Timer.svelte
+++ src/Timer.svelte
@@ -18,52 +18,46 @@
 	///////////////////////////////////////////////////////
 
 	let display = $timerInterval
-	let countdown = new Date()
-	let elapsed = false
-	let stopTimer
+	let countdown : Date | null;
 	let sound : Howl
 
-	const started = () => stopTimer
-	
-	export const start = async () => {
-		if(started()) return
+	let started : boolean
+	let elapsed : boolean
 
-		elapsed = false
+	timer.subscribe(now => {
+		started = !!countdown
+		elapsed = started && countdown < now
+
+		display = started
+			? Math.max(0, Math.min($timerInterval, Math.round((countdown.getTime() - now.getTime()) / 1000)))
+			: $timerInterval
+
+		if(elapsed && sound) {
+			sound.play()
+			sound = null
+		}
+	})
+
+	export const start = () => {
 		countdown = new Date()
 		countdown.setSeconds(countdown.getSeconds() + $timerInterval)
-		sound = new Howl({
-			src: ['elapsed.mp3']
-		});
-		
-		stopTimer = timer.subscribe(d => {
-			if(countdown <= d)
-				stop(true)
-			else
-				display = Math.min($timerInterval, Math.round((countdown.getTime() - d.getTime()) / 1000))
-		})
+		sound = new Howl({ src: ['elapsed.mp3'] })
 	}
-	export const stop = (hasElapsed = false) => {
-		if(stopTimer) {
-			stopTimer()
-			stopTimer = null
-		} 
-		if(hasElapsed && sound) sound.play()
-
-		elapsed = hasElapsed
-		display = $timerInterval
+	export const stop = () => {
+		countdown = null
 	}
 
-	const newInterval = async (e) => {
-		if(stopTimer) stop()
+	const intervalChanged = e => {
+		stop()
 		timerInterval.set(parseInt((e.target as HTMLInputElement).value))
 	}
 
 </script>
 
 <div class="timer">
-	<div>Timer <input type="number" class="length" value={display} on:input={newInterval} min=1> s</div>
+	<div>Timer <input type="number" class="length" value={display} on:input={intervalChanged} min=1> s</div>
 	<div>
-		<button class="start" class:alt={started() && $alternate} on:click={start}>Start</button> 
+		<button class="start" class:alt={started && !elapsed && $alternate} on:click={start}>Start</button> 
 		<button class="stop" class:alt={elapsed && $alternate} on:click={() => stop()}>Stop</button>
 	</div>
 </div>
